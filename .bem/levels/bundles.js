48788b02a3327fba278dd8bfab879cc29a9ce365
Anton Winogradov <winogradovaa@gmail.com>
2014-09-14 10:21:16 +0400

    add libs for tests
    add sets building
    
    update gitignore
    
    Add libs for make sets and testing

--- .bem/levels/bundles.js
+++ .bem/levels/bundles.js
@@ -1,21 +1,19 @@
-var dirs            = require('../../package.json')._directories,
-    path            = require('path'),
-    environ         = require('bem-environ'),
-    getTechResolver = environ.getTechResolver,
-
-    PRJ_ROOT        = environ.PRJ_ROOT,
-    PRJ_TECHS       = path.resolve(PRJ_ROOT, '.bem/techs'),
-    BEMCORE_TECHS   = environ.getLibPath('bem-core', '.bem/techs');
+var environ         = require('bem-environ'),
+    BEMCORE_TECHS   = environ.getLibPath('bem-core', '.bem/techs'),
+    BEMPR_TECHS     = environ.getLibPath('bem-pr', 'bem/techs'),
+    getTechResolver = environ.getTechResolver;
 
 exports.baseLevelPath = require.resolve('./blocks');
 
 exports.getTechs = function() {
     var techs = this.__base();
 
-    // Use techs from lib bem-core
     ['browser.js+bemhtml', 'html'].forEach(getTechResolver(techs, BEMCORE_TECHS));
+
+    ['phantomjs', 'spec.bemjson.js'].forEach(getTechResolver(techs, BEMPR_TECHS));
 
     return techs;
 };
 
+// Create bundles in bemjson.js tech
 exports.defaultTechs = ['bemjson.js'];
