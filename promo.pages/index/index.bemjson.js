d95db4c2e984c7f05de2d4492d7db5fbed5da7c0
Anton Winogradov <winogradovaa@gmail.com>
2014-10-04 19:17:26 +0400

    add block gh-ribbon

--- promo.pages/index/index.bemjson.js
+++ promo.pages/index/index.bemjson.js
@@ -10,19 +10,8 @@
             elem : 'header',
             content : [
                 {
-                    block : 'link',
-                    url : 'https://github.com/verybigman/bem-grid/tree/master',
-                    content : [
-                        {
-                            tag : 'img',
-                            attrs : {
-                                src : 'https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67',
-                                alt : 'Fork me on GitHub',
-                                style : 'position: absolute; top: 0; right: 0; border: 0;',
-                                'data-canonical-src' : 'https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'
-                            }
-                        }
-                    ]
+                    block : 'gh-ribbon',
+                    url : 'https://github.com/verybigman/bem-grid'
                 },
                 {
                     block : 'heading', lvl : 1, content : 'Flexbox bem-grid'
