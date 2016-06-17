ec3ad2e783bc7d2ec618b61cc2237df2cc8df25a
awinogradov <winogradovaa@gmail.com>
2016-06-17 17:47:01 +0300

    fix(row): vars and calc

--- engines/postcss/index.js
+++ engines/postcss/index.js
@@ -1,14 +1,19 @@
-var postcss = require('postcss');
-var src = require('../src');
-var plugins = require('../plugins');
+'use strict';
 
-module.exports = postcss.plugin('postcss-bem-grid', function (opts) {
-    opts = opts || {};
-    return function(css) {
-        return new Promise(function(resolve) {
-            postcss(plugins(opts)).process(src).then(function(gridResult) {
-                css.prepend(gridResult.css);
-            }).then(resolve);
-        });
-    }
+const postcss = require('postcss');
+const src = require('../src');
+const plugins = require('../plugins');
+
+module.exports = postcss.plugin('postcss-sharps', function (opts) {
+  opts = opts || {};
+  opts.columns = opts.columns || 12;
+  opts.columnsExceptOne = opts.columns - 1;
+  
+  return function(css) {
+    return new Promise(function(resolve) {
+      postcss(plugins(opts)).process(src).then(function(gridResult) {
+        css.prepend(gridResult.css);
+      }).then(resolve);
+    });
+  }
 });
